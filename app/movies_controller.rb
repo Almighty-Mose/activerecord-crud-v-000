726b9ad24095869c62c7c211ae214a73c20215a5
Branden Mosley <branden.mosley1337@gmail.com>
2018-04-22 18:14:11 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -22,14 +22,14 @@ def can_be_created_with_a_hash_of_attributes
   movie = Movie.create(attributes)
 end
 
-def can_be_created_in_a_block(args = __)
+def can_be_created_in_a_block(args = nil)
   Movie.create do |m|
-    __
+    m.
   end
 end
 
 def can_get_the_first_item_in_the_database
-  __
+  Movie.find(1)
 end
 
 def can_get_the_last_item_in_the_database
